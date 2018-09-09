e1f64b4fbbd4a672f94c2300f1e6aab631f04456
高森 <gaosen@tuya.com>
2018-09-09 14:32:11 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+  name='yindl',
+  version='0.1',
+  author='gaosen',
+  author_email='0x5e@sina.cn',
+  description='',
+  url='https://github.com/0x5e/yindl-knx',
+  packages=['src'],
+  install_requires=[
+    'construct>=2.9',
+  ]
+)
