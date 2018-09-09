def65d222b49f5a8f4046bf780cf4b32060d1798
高森 <gaosen@tuya.com>
2018-09-10 00:45:42 +0800

    runloop

--- yindl/yindl.py
+++ yindl/yindl.py
@@ -106,11 +106,8 @@ class YindlClient():
       self.send_pkg({'type': 'Heartbeat', 'data': [0x7b]})
 
   def start(self):
-    try:
-      loop = asyncio.get_event_loop()
-      if loop.is_running():
-        self.run(loop)
-      else:
-        loop.run_until_complete(self.run(loop))
-    finally:
-      loop.close()
+    loop = asyncio.get_event_loop()
+    if loop.is_running():
+      self.run(loop)
+    else:
+      loop.run_until_complete(self.run(loop))
