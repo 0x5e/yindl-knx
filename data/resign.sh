8f85f06744179a054d476cd2fb8ee77ca0052197
gaosen <0x5e@sina.cn>
2022-03-22 19:34:35 +0800

    add data

---
+++ data/resign.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+fastlane sigh resign ./EControl.ipa --new_bundle_id com.0x5e.EControl --entitlements ./entitlements.plist --provisioning_profile ./77605b93-4267-404c-b106-4248052421e5.mobileprovision --signing_identity 19572BE3BCA3FEC7726FCB29B506968E63734A38
