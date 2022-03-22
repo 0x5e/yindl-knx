c1a50646c30e22794ca9fa14426ab6d413e6514e
gaosen <0x5e@sina.cn>
2022-03-22 18:38:51 +0800

    load dylib on launch

--- EControl/EControl/main.m
+++ EControl/EControl/main.m
@@ -6,37 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
-
-#import <dlfcn.h>
-#import <mach/mach.h>
-#import <mach-o/loader.h>
-#import <mach-o/dyld.h>
+//#import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
 //        appDelegateClassName = NSStringFromClass([AppDelegate class]);
-        
-        
-        NSString * dylibName = @"libEControl";
-        NSString * path = [[NSBundle mainBundle] pathForResource:dylibName ofType:@"dylib" inDirectory:@"Frameworks"];
-        if (dlopen(path.UTF8String, RTLD_NOW) == NULL){
-            NSLog(@"dlopen failed ，error %s", dlerror());
-            return 0;
-        };
-        
-        uint32_t dylib_count = _dyld_image_count();
-        uint64_t slide = 0;
-        for (int i = 0; i < dylib_count; i ++) {
-            const char * name = _dyld_get_image_name(i);
-            if ([[NSString stringWithUTF8String:name] isEqualToString:path]) {
-                slide = _dyld_get_image_vmaddr_slide(i);
-            }
-        }
-        assert(slide != 0);
-        
         appDelegateClassName = @"AppDelegate";
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
